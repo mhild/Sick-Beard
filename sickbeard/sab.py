c1d38078a580aec901491007817f2d90a949a69d
mhild <mhild@users.noreply.github.com>
2014-03-15 18:59:35 +0100

    Update sab.py

--- sickbeard/sab.py
+++ sickbeard/sab.py
@@ -34,12 +34,55 @@ from sickbeard.common import USER_AGENT
 from sickbeard import logger
 from sickbeard.exceptions import ex
 
+import struct, socket, sys, time, os
+
+#Test Connection function
+def TestCon(host,port):
+        s = socket.socket()
+        s.settimeout(1)
+        try:
+        	s.connect((host,port))
+                return "Up"
+                s.close()
+        except socket.error, e:
+       		return "Down"
+
+#Wake function
+def WakeOnLan(ethernet_address):
+
+	addr_byte = ethernet_address.split(':')
+      	hw_addr = struct.pack('BBBBBB', int(addr_byte[0], 16),
+        int(addr_byte[1], 16),
+        int(addr_byte[2], 16),
+        int(addr_byte[3], 16),
+        int(addr_byte[4], 16),
+        int(addr_byte[5], 16))
+                     
+        # Build the Wake-On-LAN "Magic Packet"...
+                        
+        msg = '\xff' * 6 + hw_addr * 16
+                           
+        # ...and send it to the broadcast address using UDP
+                              
+        ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        ss.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        ss.sendto(msg, ('<broadcast>', 9))
+        ss.close()
+
 def sendNZB(nzb):
     """
     Sends an NZB to SABnzbd via the API.
     
     nzb: The NZBSearchResult object to send to SAB
     """
+
+    i=1
+    logger.log(u"Testing SABnzb connectivity...")
+    while TestCon("192.168.1.31",9090)=="Down" and i<4:
+    	i=i+1
+    	logger.log(u"sabnzbd host seems to be down - sending magic packet")
+    	WakeOnLan('00:0c:76:50:a9:66')    
+	time.sleep(20)
 
     # set up a dict with the URL params in it
     params = {}
